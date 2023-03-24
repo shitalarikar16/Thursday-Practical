f7f331790d7994e0855bc2ef4b0766439aa5d9ec
shitalarikar16 <128776746+shitalarikar16@users.noreply.github.com>
2023-03-24 11:21:37 +0530

    Add files via upload

---
+++ 23 march/EvenOdd.java
@@ -0,0 +1,17 @@
+class EvenOdd
+{
+	
+	public static void main(String []args)
+	{
+		int a=29;
+		if(a%2==0)
+		{
+		  System.out.println("Number is even");	
+		}
+		else
+		{
+			System.out.println("Number is odd");
+		}
+	}
+}
+
